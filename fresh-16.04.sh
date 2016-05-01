c57d6ac907c2ed1d8a75c56936ebdb02a97f2417
Bernie <b4456609@gmail.com>
2016-05-01 10:29:11 +0800

    fix echo space

--- fresh-16.04.sh
+++ fresh-16.04.sh
@@ -1,74 +1,74 @@
 #!/bin/bash
 
-echo"#java8 ppa"
+echo "#java8 ppa"
 sudo add-apt-repository -y ppa:webupd8team/java
 
-echo"#ansible ppa"
+echo "#ansible ppa"
 sudo apt-add-repository -y ppa:ansible/ansible
 
-echo"#spotify"
-echo"# 1. Add the Spotify repository signing key to be able to verify downloaded packages"
+echo "#spotify"
+echo "# 1. Add the Spotify repository signing key to be able to verify downloaded packages"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 
-echo"# 2. Add the Spotify repository"
+echo "# 2. Add the Spotify repository"
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
-echo"# Google Public Key downloading #"
+echo "# Google Public Key downloading #"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-echo"# Adding Google Chrome Repository #"
+echo "# Adding Google Chrome Repository #"
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
-echo"# sublime"
+echo "# sublime"
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 
-echo"# Add Virtualbox Repository"
+echo "# Add Virtualbox Repository"
 $ sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list"
-echo"# Import Oracle Public Key"
+echo "# Import Oracle Public Key"
 $ wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 
-echo"# Repository update #"
+echo "# Repository update #"
 sudo apt-get update
 sudo apt upgrade -y
 
-echo"#Install Google Chrome"
+echo "#Install Google Chrome"
 sudo apt-get install -y google-chrome-stable
 
-echo"# sublime"
+echo "# sublime"
 sudo apt-get install -y sublime-text-installer
 
-echo"# fcitx"
+echo "# fcitx"
 sudo apt-get install -y fcitx fcitx-chewing
 sudo apt-get install -y build-essential libgtk2.0-dev
 
-echo"# Setting Fcitx as the default input method!"
+echo "# Setting Fcitx as the default input method!"
 im-switch -s fcitx -z default
 
 sudo gcc -shared -o sublime/libsublime-imfix.so sublime/sublime_imfix.c  `pkg-config --libs --cflags gtk+-2.0` -fPIC
 sudo mv sublime/libsublime-imfix.so  /opt/sublime_text
 
-echo"# replace sublime text "
+echo "# replace sublime text "
 sudo rm /usr/share/applications/sublime-text.desktop
 sudo cp sublime/sublime-text.desktop /usr/share/applications
 
-echo"# replace sublime text bin"
+echo "# replace sublime text bin"
 sudo rm /usr/bin/subl
 sudo cp sublime/subl /usr/bin
 
-echo"#spotify"
-echo"# 4. Install Spotify"
+echo "#spotify"
+echo "# 4. Install Spotify"
 sudo apt-get install spotify-client
 
-echo"#nodejs"
+echo "#nodejs"
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 
-echo"#java8 installer"
+echo "#java8 installer"
 echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
 sudo apt-get install -y oracle-java8-installer
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> $HOME/.profile
 
-echo"#gradle"
+echo "#gradle"
 MAIN=2
 VERSION=13
 wget https://services.gradle.org/distributions/gradle-$MAIN.$VERSION-bin.zip
@@ -83,54 +83,54 @@ echo "org.gradle.daemon=true" >> $HOME/.gradle/gradle.properties
 source ~/.profile
 
 
-echo"#GIT"
+echo "#GIT"
 sudo apt-get install -y git git-cola
 
 git config --global user.name "Bernie"
 git config --global user.email "b4456609@gmail.com"
 
-echo"#nodejs"
+echo "#nodejs"
 sudo apt-get install -y nodejs
 
 
-echo"#ansible"
+echo "#ansible"
 sudo apt-get install -y ansible
 
-echo"# docker"
+echo "# docker"
 curl -fsSL https://get.docker.com/ | sh
 sudo gpasswd -a ${USER} docker
 
-echo"# Install Media Codecs"
+echo "# Install Media Codecs"
 sudo apt-get install -y ubuntu-restricted-extras
 
-echo"# Install video player"
+echo "# Install video player"
 sudo apt-get install -y mpv
 
-echo"# Install filezilla"
+echo "# Install filezilla"
 sudo apt-get install -y filezilla
 
-echo"# Install VirtualBox"
+echo "# Install VirtualBox"
 sudo apt-get install -y virtualbox-5.0
 
-echo"# dev tool"
+echo "# dev tool"
 sudo apt-get install -y zsh
 
-echo"# clean tool"
+echo "# clean tool"
 sudo apt-get install -y bleachbit
 
-echo"# vagrant "
-echo"# Dependencies"
+echo "# vagrant "
+echo "# Dependencies"
 sudo apt-get install libqt4-opengl -y
 VAGRANT="https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb"
-echo"# Download vagrant"
+echo "# Download vagrant"
 wget "$VAGRANT" -P /tmp
-echo"# Install vagrant"
+echo "# Install vagrant"
 sudo dpkg -i "/tmp/$(basename $VAGRANT)"
 
 
-echo"# Disable Guest Session on Unity"
+echo "# Disable Guest Session on Unity"
 sudo sh -c "echo 'allow-guest=false' >> /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf"
-echo"# Enable one-click minimize"
+echo "# Enable one-click minimize"
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
 
 echo "Cleaning Up" &&
@@ -139,18 +139,18 @@ sudo apt-get -y autoremove &&
 sudo apt-get -y autoclean &&
 sudo apt-get -y clean
 
-echo"# install oh-my-zsh"
+echo "# install oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo"# intellij eclipse robomongo docker-compose"
-echo"# https://robomongo.org/download"
-echo"# https://docs.docker.com/compose/install/"
-echo"# https://www.jetbrains.com/idea/#chooseYourEdition"
-echo"# http://www.eclipse.org/downloads/packages/eclipse-ide-java-ee-developers/mars2"
-echo"# http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz&r=1"
+echo "# intellij eclipse robomongo docker-compose"
+echo "# https://robomongo.org/download"
+echo "# https://docs.docker.com/compose/install/"
+echo "# https://www.jetbrains.com/idea/#chooseYourEdition"
+echo "# http://www.eclipse.org/downloads/packages/eclipse-ide-java-ee-developers/mars2"
+echo "# http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz&r=1"
 
-echo"# sublime"
-echo"# https://packagecontrol.io/installation"
-echo"# sync settings"
-echo"# https://gist.github.com/b4456609/f7742fe74eb8308be29d"
+echo "# sublime"
+echo "# https://packagecontrol.io/installation"
+echo "# sync settings"
+echo "# https://gist.github.com/b4456609/f7742fe74eb8308be29d"
 
